ed188131e26524dd4e607a64c2d3982199e03ff6
Marcos-SCO <marcos_sco@outlook.com>
2021-07-26 17:58:20 -0300

    random voices

--- js/selecao.js
+++ js/selecao.js
@@ -1,43 +1,62 @@
 const selection = document.querySelector('.selection');
 
-const clickedTrays = [];
+const clickedTrays = {};
 
-selection.addEventListener('click', function (e) {
+function randomNumber(multiplyBy, init = 1) {
+	return Math.floor(Math.random() * multiplyBy) + init;
+}
+
+selection.addEventListener('click', characterElement);
+
+function characterElement(e) {
 	e.preventDefault();
 	e.stopPropagation();
 	const clickedParent = e.target.parentElement;
 	const isCharacterElement = clickedParent.hasAttribute('data-character');
 
-	if (isCharacterElement) {
-		const characterName = clickedParent.getAttribute('data-character');
-		console.log(characterName);
+	if (isCharacterElement) displayCharacter(clickedParent);
+}
 
-		e.preventDefault();
+function displayCharacter(clickedParent) {
+	const characterName = clickedParent.getAttribute('data-character');
+	// clickedParent.preventDefault();
 
-		document.getElementById('img').innerHTML = `<img src="img/gifs/${characterName}.gif">`;
+	document.getElementById('img').innerHTML = `<img src="img/gifs/${characterName}.gif">`;
 
-		document.querySelector('figcaption').style.display = 'block';
+	document.querySelector('figcaption').style.display = 'block';
 
-		document.getElementById('face').innerHTML = `<img src="img/gifs/${characterName}_face.gif">`;
+	document.getElementById('face').innerHTML = `<img src="img/gifs/${characterName}_face.gif">`;
 
-		document.getElementById('p').innerHTML = characterName.replace('_', ' ');
+	document.getElementById('p').innerHTML = characterName.replace('_', ' ');
 
-		document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay volume="0.2"></audio>';
+	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay volume="0.2"></audio>';
 
-		const randomSound = Math.floor(Math.random() * 8) + 1;
-		if (clickedTrays.includes(characterName)) {
-			console.log(characterName)
-		}
+	sortAndPlaySound(characterName, clickedParent);
 
-		clickedTrays.push(characterName);
+}
 
+function sortAndPlaySound(characterName, clickedParent) {
+	let randomSound = randomNumber(8);
+
+	const CharacterNotExitsInArray = !clickedTrays[characterName];
+	if (CharacterNotExitsInArray) clickedTrays[characterName] = [];
+
+	const isRepeatedSound = clickedTrays[characterName].includes(randomSound);
+
+	if (clickedTrays[characterName].length == 8) {
+		clickedTrays[characterName] = [];
+	}
+
+	if (isRepeatedSound) {
+		displayCharacter(clickedParent);
+		return;
+	}
+
+	if (!isRepeatedSound) {
+		clickedTrays[characterName].push(randomSound);
 		document.getElementById('som').innerHTML = `<audio src="audio/${characterName}/${randomSound}.wav" autoplay></audio>`;
 	}
-});
-
-
-
-
+}
 
 
 
