a303d95238837be2e3aa61efe1de4df2786223ab
Marcos-SCO <marcos_sco@outlook.com>
2021-07-26 14:28:12 -0300

    initial changes

--- index.php
+++ index.php
@@ -5,31 +5,28 @@
 	<meta charset="UTF-8">
 	<title>Marvel VS Capcom</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="model/css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 	<header>
-		<a href=index.php><img src="model/img/MVC_logo.png"></a>
+		<a href=index.php><img src="img/MVC_logo.png"></a>
 	</header>
-    
-    <?php
-	$select = isset($_GET['op']) ? $_GET['op'] : '';
-	
-	if($select === 'selecao'){
-	  include "model/templates/select.html";	
-	}
-    else{
-		include "model/templates/introducao.html";
-	}	
-	?>
-  
-	
-	<footer>
-		<p><a target="_blank" href="https://mscwps.000webhostapp.com/">&copy; MSC - 2019</a></p>
-	</footer>
 
-	
+	<?php
+	$select = isset($_GET['op']) ? $_GET['op'] : '';
+
+	if ($select === 'selecao') {
+		include "templates/select.html";
+	} else {
+		include "templates/introducao.html";
+	}
+	?>
+
+	<footer>
+		<p><a target="_blank" href="https://mscwps.000webhostapp.com/">2019 | <?= date('Y') ?> &copy; Marcos dos Santos Carvalho </a></p>
+		<p></p>
+	</footer>
 </body>
 
 </html>
