cc963e8738407f96a0353940d49010afd657d2d9
Marcos-SCO <marcos_sco@outlook.com>
2021-07-26 15:20:01 -0300

    name changes

--- js/selecao.js
+++ js/selecao.js
@@ -1,257 +1,307 @@
-document.getElementById('chunli').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/chun-li.gif">';
+// document.getElementById('chunli').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/chun-li.gif">';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/chunli_face.gif">';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/chunli_face.gif">';
 
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('p').innerHTML = 'Chun-li';
+// 	document.getElementById('p').innerHTML = 'Chun-li';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/chunli.wav" autoplay></audio>';
-}
+// 	document.getElementById('som').innerHTML = '<audio src="audio/chunli.wav" autoplay></audio>';
+// }
 
-//Ryu
-document.getElementById('ryu').onclick = function () {
-	
-	document.getElementById('p').innerHTML = 'Ryu'; 
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/ryu.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// //Ryu
+// document.getElementById('ryu').onclick = function () {
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/ryu_face.gif">';
+// 	document.getElementById('p').innerHTML = 'Ryu';
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/ryu.gif">';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/ryu.wav" autoplay></audio>';
-}
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/ryu_face.gif">';
 
-//zangief
-document.getElementById('zangif').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/zangief.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/zangif_face.gif">';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/ryu.wav" autoplay></audio>';
+// }
 
-	document.getElementById('p').innerHTML = 'Zangief';
+// //zangief
+// document.getElementById('zangif').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/zangief.gif">';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/zangif.wav" autoplay></audio>';
-}
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/zangif_face.gif">';
 
-//Morrigan
-document.getElementById('morrigan').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/morrigan.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('p').innerHTML = 'Zangief';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/morrigan_face.gif">';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('p').innerHTML = 'Morrigan';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/zangif.wav" autoplay></audio>';
+// }
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// //Morrigan
+// document.getElementById('morrigan').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/morrigan.gif">';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/morrigan.wav" autoplay></audio>';
-}
+// 	document.querySelector('figcaption').style.display = 'block';
 
-//Comando
-document.getElementById('comando').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/comando.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/morrigan_face.gif">';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/comando_face.gif">';
+// 	document.getElementById('p').innerHTML = 'Morrigan';
 
-	document.getElementById('p').innerHTML = 'Captain Comando';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/morrigan.wav" autoplay></audio>';
+// }
 
-	document.getElementById('som').innerHTML = '<audio src="audio/comando.wav" autoplay></audio>';
-}
+// //Comando
+// document.getElementById('comando').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/comando.gif">';
 
-//Megaman
-document.getElementById('megaman').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/megaman.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/megaman_face.gif">';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/comando_face.gif">';
 
-	document.getElementById('p').innerHTML = 'Megaman';
+// 	document.getElementById('p').innerHTML = 'Captain Comando';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/megaman.wav" autoplay></audio>';
-}
+// 	document.getElementById('som').innerHTML = '<audio src="audio/comando.wav" autoplay></audio>';
+// }
 
-//Strider
-document.getElementById('strider').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/strider.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// //Megaman
+// document.getElementById('megaman').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/megaman.gif">';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/strider_face.gif">';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('p').innerHTML = 'Strider';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/megaman_face.gif">';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('p').innerHTML = 'Megaman';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/strider.wav" autoplay></audio>';
-}
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-//Spiderman
-document.getElementById('spiderman').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/spiderman.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/megaman.wav" autoplay></audio>';
+// }
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/spider_face.gif">';
+// //Strider
+// document.getElementById('strider').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/strider.gif">';
 
-	document.getElementById('p').innerHTML = 'Spiderman';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/strider_face.gif">';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/spider.wav" autoplay></audio>';
-}
+// 	document.getElementById('p').innerHTML = 'Strider';
 
-//Jin
-document.getElementById('jin').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/jin.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/jin_face.gif">';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/strider.wav" autoplay></audio>';
+// }
 
-	document.getElementById('p').innerHTML = 'Jin';
+// //Spiderman
+// document.getElementById('spiderman').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/spiderman.gif">';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/jin.wav" autoplay></audio>';
-}
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/spider_face.gif">';
 
-//America
-document.getElementById('america').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/america.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('p').innerHTML = 'Spiderman';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/captain_face.gif">';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('p').innerHTML = 'Captain America';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/spider.wav" autoplay></audio>';
+// }
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// //Jin
+// document.getElementById('jin').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/jin.gif">';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/america.wav" autoplay></audio>';
-}
+// 	document.querySelector('figcaption').style.display = 'block';
 
-//Venom
-document.getElementById('venom').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/venom.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/jin_face.gif">';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/venom_face.gif">';
+// 	document.getElementById('p').innerHTML = 'Jin';
 
-	document.getElementById('p').innerHTML = 'Venom';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/jin.wav" autoplay></audio>';
+// }
 
-	document.getElementById('som').innerHTML = '<audio src="audio/venom.wav" autoplay></audio>';
-}
+// //America
+// document.getElementById('america').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/america.gif">';
 
-//Venom
-document.getElementById('hulk').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/hulk.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/hulk_face.gif">';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/captain_face.gif">';
 
-	document.getElementById('p').innerHTML = 'Hulk';
+// 	document.getElementById('p').innerHTML = 'Captain America';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/hulk.wav" autoplay></audio>';
-}
+// 	document.getElementById('som').innerHTML = '<audio src="audio/america.wav" autoplay></audio>';
+// }
 
-//Gambit
-document.getElementById('gambit').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/gambit.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// //Venom
+// document.getElementById('venom').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/venom.gif">';
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/gambit_face.gif">';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('p').innerHTML = 'Gambit';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/venom_face.gif">';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('p').innerHTML = 'Venom';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/gambit.wav" autoplay></audio>';
-}
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-//Warmachine
-document.getElementById('warmachine').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/warmachine.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/venom.wav" autoplay></audio>';
+// }
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/warmachine_face.gif">';
+// //Venom
+// document.getElementById('hulk').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/hulk.gif">';
 
-	document.getElementById('p').innerHTML = 'Warmachine';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/hulk_face.gif">';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/warmachine.wav" autoplay></audio>';
-}
+// 	document.getElementById('p').innerHTML = 'Hulk';
 
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
 
-//Warmachine
-document.getElementById('wolverine').onclick = function () {
-	document.getElementById('img').innerHTML =
-		'<img src="img/gifs/wolverine.gif">';
-	
-	document.querySelector('figcaption').style.display = 'block';
+// 	document.getElementById('som').innerHTML = '<audio src="audio/hulk.wav" autoplay></audio>';
+// }
 
-	document.getElementById('face').innerHTML =
-		'<img src="img/gifs/wolverine_face.gif">';
+// //Gambit
+// document.getElementById('gambit').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/gambit.gif">';
 
-	document.getElementById('p').innerHTML = 'wolverine';
+// 	document.querySelector('figcaption').style.display = 'block';
 
-	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/gambit_face.gif">';
 
-	document.getElementById('som').innerHTML = '<audio src="audio/wolverine.wav" autoplay></audio>';
-}
+// 	document.getElementById('p').innerHTML = 'Gambit';
+
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+
+// 	document.getElementById('som').innerHTML = '<audio src="audio/gambit.wav" autoplay></audio>';
+// }
+
+// //Warmachine
+// document.getElementById('warmachine').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/warmachine.gif">';
+
+// 	document.querySelector('figcaption').style.display = 'block';
+
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/warmachine_face.gif">';
+
+// 	document.getElementById('p').innerHTML = 'Warmachine';
+
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+
+// 	document.getElementById('som').innerHTML = '<audio src="audio/warmachine.wav" autoplay></audio>';
+// }
+
+
+// //Warmachine
+// document.getElementById('wolverine').onclick = function () {
+// 	document.getElementById('img').innerHTML =
+// 		'<img src="img/gifs/wolverine.gif">';
+
+// 	document.querySelector('figcaption').style.display = 'block';
+
+// 	document.getElementById('face').innerHTML =
+// 		'<img src="img/gifs/wolverine_face.gif">';
+
+// 	document.getElementById('p').innerHTML = 'wolverine';
+
+// 	document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+
+// 	document.getElementById('som').innerHTML = '<audio src="audio/wolverine.wav" autoplay></audio>';
+// }
+
+const selection = document.querySelector('.selection');
+
+selection.addEventListener('click', function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	const clickedParent = e.target.parentElement;
+	const isCharacterElement = clickedParent.hasAttribute('data-character');
+
+	if (isCharacterElement) {
+		const characterName = clickedParent.getAttribute('data-character');
+		console.log(characterName);
+
+		e.preventDefault();
+
+		document.getElementById('img').innerHTML = `<img src="img/gifs/${characterName}.gif">`;
+
+		document.querySelector('figcaption').style.display = 'block';
+
+		document.getElementById('face').innerHTML = `<img src="img/gifs/${characterName}_face.gif">`;
+
+		document.getElementById('p').innerHTML = characterName.replace('_', ' ');
+
+		document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+
+		const randomSound = Math.floor(Math.random() * 8) + 1;
+
+		document.getElementById('som').innerHTML = `<audio src="audio/${characterName}/${randomSound}.wav" autoplay></audio>`;
+	}
+});
+
+// characters.forEach(character => {
+// 	character.onclick = (e) => {
+// 		e.preventDefault();
+
+// 		document.getElementById('img').innerHTML =
+// 			'<img src="img/gifs/wolverine.gif">';
+
+// 		document.querySelector('figcaption').style.display = 'block';
+
+// 		document.getElementById('face').innerHTML =
+// 			'<img src="img/gifs/wolverine_face.gif">';
+
+// 		document.getElementById('p').innerHTML = 'wolverine';
+
+// 		document.getElementById('coin').innerHTML = '<audio src="audio/select.mp3" autoplay></audio>';
+
+// 		document.getElementById('som').innerHTML = '<audio src="audio/wolverine.wav" autoplay></audio>';
+// 	}
+// });
 
 
 
